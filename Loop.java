4ad983dcf7c83c9b9d777d2fc2c36b45deff9a2d
vaibhavgoswami87 <vaibhavgoswami055@gmail.com>
2025-06-11 10:16:38 +0530

    loop program add

---
+++ Loop.java
@@ -0,0 +1,10 @@
+//print program from 1 to 10 using for loop
+
+class Loop {
+
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.err.println("i " + i);
+        }
+    }
+}
