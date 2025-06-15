399eba5c5f374cda7eb31640ee55c838cfa8c4d5
vaibhavgoswami87 <vaibhavgoswami055@gmail.com>
2025-06-15 10:52:28 +0530

    input java file add

---
+++ InputJava.java
@@ -0,0 +1,15 @@
+
+// Input in Java using Scanner Class
+// code by:vaibhav pari
+
+import java.util.Scanner;
+
+public class InputJava {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        System.out.print("Enter a number:");
+        int num = scn.nextInt();
+        System.out.println("You Entered:" + num);
+    }
+}
