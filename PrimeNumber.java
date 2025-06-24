8a0e0e34b2f2bc0981136af3829a157b5edd64fa
vaibhavgoswami87 <vaibhavgoswami055@gmail.com>
2025-06-24 19:21:01 +0530

    prime number program updated

--- PrimeNumber.java
+++ PrimeNumber.java
@@ -7,7 +7,7 @@ class PrimeNumber {
         if (n <= 1) {
             return false;
         }
-        for (int i = 2; i <= n - 1; i++) {
+        for (int i = 2; i <= Math.sqrt(n); i++) {
             if (n % 2 == 0) {
                 return false;
             }
